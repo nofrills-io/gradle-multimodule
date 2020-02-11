4d73643ba3f3133804ae9004f0e842f319908f2f
Mateusz Armatys <mateusz.armatys@nofrills.io>
2020-02-11 15:54:57 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    `java-gradle-plugin`
+    id("org.jetbrains.kotlin.jvm") version "1.3.61"
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+gradlePlugin {
+    val greeting by plugins.creating {
+        id = "io.nofrills.multimodule"
+        implementationClass = "io.nofrills.multimodule.MultimodulePlugin"
+    }
+}
+
+// Add a source set for the functional test suite
+val functionalTestSourceSet = sourceSets.create("functionalTest") {
+}
+
+gradlePlugin.testSourceSets(functionalTestSourceSet)
+configurations.getByName("functionalTestImplementation").extendsFrom(configurations.getByName("testImplementation"))
+
+// Add a task to run the functional tests
+val functionalTest by tasks.creating(Test::class) {
+    testClassesDirs = functionalTestSourceSet.output.classesDirs
+    classpath = functionalTestSourceSet.runtimeClasspath
+}
+
+val check by tasks.getting(Task::class) {
+    // Run the functional tests as part of `check`
+    dependsOn(functionalTest)
+}
