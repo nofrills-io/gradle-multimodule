a52bbefcd5db93d3762bd5b451977ac488c8d058
Mateusz Armatys <mateusz@nofrills.io>
2020-03-17 16:34:34 +0100

    Update required kotlin version; prepare 0.2.2-dev

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,7 +74,7 @@ val check by tasks.getting(Task::class) {
 
 
 group = "io.nofrills"
-version = "0.2.1"
+version = "0.2.2-dev"
 
 publishing {
     repositories {
--- buildSrc/src/main/java/project_settings.kt
+++ buildSrc/src/main/java/project_settings.kt
@@ -4,7 +4,7 @@ import org.gradle.api.artifacts.ExternalModuleDependency
 object Ver {
     object AndroidBuildTools : VersionConstraintAction(preferred = "3.6.1", required = "[3.5,4.0)")
     object Dokka : VersionConstraintAction(preferred = "0.10.1", required = "[0.10,1.0)")
-    object Kotlin : VersionConstraintAction(preferred = "1.3.70", required = "[1.3,2.0)")
+    object Kotlin : VersionConstraintAction(preferred = "1.3.70", required = "[1.3.20,2.0)")
 
     abstract class VersionConstraintAction(private val preferred: String, private val required: String) :
         Action<ExternalModuleDependency> {
